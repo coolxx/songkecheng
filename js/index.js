fe4a25a26e64f882975805f147440cc3e4e0e3eb
Your Name <you@example.com>
2017-10-31 14:42:06 +0800

    ddd
    sss

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(function(){
+	if($(window).width()<640){
+			(function (win,doc){
+				function change(){
+					doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/320+'px';
+				}
+				change();
+				win.addEventListener('resize',change,false);
+			})(window,document);
+		}
+	$(window).resize(function(){
+		if($(window).width()<640){
+			(function (win,doc){
+				function change(){
+					doc.documentElement.style.fontSize=20*doc.documentElement.clientWidth/320+'px';
+				}
+				change();
+				win.addEventListener('resize',change,false);
+			})(window,document);
+		}
+	})
+
+})
